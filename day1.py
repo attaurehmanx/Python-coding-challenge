488088dd1667ff6aefa44beb60c8d050eb9e036b
ATTAUREHMAN <attaurehmanxzz@gmail.com>
2025-06-23 20:29:42 +0500

    Day 01: Hello World

---
+++ day1.py
@@ -0,0 +1,6 @@
+# Week 1: Fundamentals & Basics
+# -----------------------------
+# Day 1  – Hello Python World: Print statements, comments, basic I/O.
+
+hello = "Hello World" # first program
+print(hello)
